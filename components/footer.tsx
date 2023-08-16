e6ab2e6b8c1ea8e4fd174e455b1a05feb3e968bc
AlexKorablik <alexkorablev4@gmail.com>
2023-08-16 19:00:15 +0300

    final

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
+          &copy; 2023 Sneakers, Inc. All rights reserved (To SneakerHeads ).
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
