c4804455322f65894fa172c9f7ff4b8245bac379
AlexKorablik <110320085+AlexKorablik@users.noreply.github.com>
2023-08-16 19:57:48 +0300

    Update page.tsx

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("7f260264-f3a3-4ed8-8a00-ac7c9be1c806");
 
   return (
     <Container>
